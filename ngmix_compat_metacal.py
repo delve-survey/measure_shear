4444d0c6c9113076008e8bcc6522471eb78677b3
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2021-12-15 18:53:34 -0600

    Reorganize, remove relative imports, other fixes

---
+++ ngmix_compat_metacal.py
@@ -0,0 +1,9 @@
+import ngmix
+
+if ngmix.__version__[0:2] == "v1":
+    NGMIX_V2 = False
+    from ngmix.fitting import format_pars
+
+else:
+    NGMIX_V2 = True
+    from ngmix.util import format_pars
