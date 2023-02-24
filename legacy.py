d39006178310234e8bb52eaa40598c3a9a7700b1
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2023-02-24 08:45:39 -0600

    Updating legacy file

--- legacy.py
+++ legacy.py
@@ -20,8 +20,10 @@ logger = logging.getLogger(__name__)
 
 def _run_mcal_one_chunk_old(meds_files, start, end, seed, mcal_config):
     """Run metcal for `meds_files` only for objects from `start` to `end`.
+
     Note that `start` and `end` follow normal python indexing conventions so
     that the list of indices processed is `list(range(start, end))`.
+
     Parameters
     ----------
     meds_files : list of str
@@ -34,6 +36,7 @@ def _run_mcal_one_chunk_old(meds_files, start, end, seed, mcal_config):
         The seed for the RNG.
     mcal_config : yaml
         The config file for the metacal run
+
     Returns
     -------
     output : np.ndarray
@@ -55,78 +58,131 @@ def _run_mcal_one_chunk_old(meds_files, start, end, seed, mcal_config):
         cat = mfiles[0].get_cat()
 
         for ind in range(start, end):
-            o = mbmeds.get_mbobs(ind)
             
+#             print("IN GAL", ind)
+            
+            #Load mbobs for fiducial run
+            o = mbmeds.get_mbobs(ind, weight_type='weight')
+            
+            #Use wcs of coadd to make gaussian pixel weights for bad mask fraction
+            if len(o[0]) == 0: 
+                continue
+            else:
+                coadd_wcs_rband = o[0][0].jacobian.get_galsim_wcs()
+             
             o = preprocess._strip_coadd(o, mcal_config) #Remove coadd since it isnt used in fitting
             o = preprocess._strip_zero_flux(o, mcal_config) #Remove any obs with zero flux
-            o = preprocess._add_zeroweights_mask(o, mcal_config) #Add mask from weights==0 condition
+            
+            #Check if missing any bands. Happens due to observation
+            #Do this once at start just to prevent computation later on
+            if preprocess._check_band_coverage(o): continue
+                
+            Ncutouts_per_band = [len(i) for i in o]
             
             #Keep only Nmax exps per band
             if mcal_config['custom']['Nexp_max'] > 0: 
-                o = preprocess._strip_Nexposures(o, rng, mcal_config) 
-                
-                
-            #Remove obs with too many bad pixs
-            if mcal_config['custom']['maxbadfrac'] > 0: 
-                o = preprocess._strip_10percent_masked(o, mcal_config)
-                
-                
-            #gauss-weighted fraction of good pixs
-            if mcal_config['custom']['goodfrac']: 
-                o = preprocess._get_masked_frac(o, mcal_config) 
-                
-                
-            #Add 180deg Symmetry of bmask
-            if mcal_config['custom']['symmetrize_mask']: 
-                o = preprocess._symmetrize_mask(o, mcal_config) 
+                o = preprocess._strip_Nexposures(o, np.random.RandomState(seed=seed), mcal_config)
+             
+            #Zero-out weights for bmask != 0
+            #from now on weights contains the bmask in it
+            o = preprocess._set_zero_weights(o, mcal_config)
+            
+            #Add 180deg Symmetry of weights (NOT UBERSEG)
+            mcal_config['custom']['symmetrize_weights'] = mcal_config['custom']['symmetrize_mask'] #Monkeypatch for now. FIX LATER
+            if mcal_config['custom']['symmetrize_weights']: 
+                o = preprocess._symmetrize_weights(o, mcal_config)             
+            
+            #Check again if missing any bands. 
+            #Happens due to weights symmetry, uberseg, or adding bmask
+            if preprocess._check_band_coverage(o): continue
+            
+            #gauss-weighted fraction of bad pixels
+            o = preprocess._get_masked_frac(o, mcal_config, coadd_wcs_rband)
+            
+            badfrac = o.meta['badfrac']
             
             
-            #Interpolate empty pixels
+            #Fill empty pixels of the cutout using interpolation + a noise image
             if mcal_config['custom']['interp_bad_pixels']: 
-                o = preprocess._fill_empty_pix(o, rng, mcal_config)
+                o = preprocess._fill_empty_pix(o, rng, mcal_config) 
+                
             
+            ##########################################################################
             
-            o = preprocess._set_zero_weights(o,  mcal_config) #Set all masked pix to have wgt=0, include mask symmetry
-
-            skip_me = False
-            for ol in o:
-                if len(ol) == 0:
-                    logger.debug(' not all bands have images - skipping!')
-                    skip_me = True
-            if skip_me:
-                continue
-
             o.meta['id'] = ind
             o[0].meta['Tsky'] = 1
             o[0].meta['magzp_ref']   = MAGZP_REF
-            o[0][0].meta['orig_col'] = cat['orig_col'][ind, 0]
-            o[0][0].meta['orig_row'] = cat['orig_row'][ind, 0]
-
-            if mcal_config['custom']['goodfrac']:
-                #put all the good_fraction numbers into one list
-                #one entry per cutout (so all bands are combined here)
-                good_frac = []
-                weight    = []
-                for _one in o:
-                    for _two in _one:
-                        good_frac.append(_two.meta['good_frac'])
-                        weight.append(_two.meta['weight'])
+#             o[0][0].meta['orig_col'] = cat['orig_col'][ind, 0]
+#             o[0][0].meta['orig_row'] = cat['orig_row'][ind, 0]
                     
             nband = len(o)
             mcal = MetacalFitter(mcal_config, nband, rng)
 
             try:
                 mcal.go([o])
-                res = mcal.result
+                
+                #Read metacal output, and get the dtype of the field.
+                #This step is done so we can add custom quantities, not computed in mcal
+                #to the final output of the metacal files
+                tmp = mcal.result
+                
+                if tmp is not None:
+                    dt = tmp.dtype.fields
+                    dt = [(i, dt[i][0]) for i in dt]
+                    
+                    #Add custom quantities for badfrac
+                    dt += [('badfrac', '>f8')]
+                    
+                    dt += [('Ncutouts_raw', '>i8', (len(o),))]
+                    
+                    #Add custom quantities for focal plane coords.
+                    #Has shape (N_bands, N_cutouts)
+                    dt += [('expnum', '>i8', (len(o), mcal_config['custom']['Nexp_max']))]
+                    dt += [('ccdnum', '>i8', (len(o), mcal_config['custom']['Nexp_max']))]
+                    dt += [('x_exp',  '>f8', (len(o), mcal_config['custom']['Nexp_max']))]
+                    dt += [('y_exp',  '>f8', (len(o), mcal_config['custom']['Nexp_max']))]
+
+                    res = np.zeros(len(tmp), dtype = dt)
+
+                    for i in tmp.dtype.names:
+                        res[i] = tmp[i]
+                        
+                    #Store number of cutouts in each band BEFORE we subsample
+                    for i in range(len(o)):
+                        res['Ncutouts_raw'][0, i] = Ncutouts_per_band[i]
+                    
+                    #Store mean bad fraction of this image to use in making cuts
+                    res['badfrac'] = badfrac
+                        
+                    #Hacking to get coadd position in results
+                    #(Mcal would just write first cutout position here by default,
+                    #but we stripped coadd so it isn't first cutout)
+                    res['x'] = cat['orig_col'][ind, 0]
+                    res['y'] = cat['orig_row'][ind, 0]
+
+                    #Set default values for these. Entries take these values
+                    #for an object that doesn't have all the cutouts we need
+                    res['expnum'] = -9999
+                    res['ccdnum'] = -9999
+                    res['x_exp']  = -9999
+                    res['y_exp']  = -9999
+
+                    for i_band in range(len(o)):
+                        for i_cutout in range(len(o[i_band])):
+                            file_path = o[i_band][i_cutout].meta['file_path']
+                            res['expnum'][0, i_band, i_cutout] = file_path.split('_')[0][1:] #Store expnum as "D00605764" -> 605764
+                            res['ccdnum'][0, i_band, i_cutout] = file_path.split('_')[2][1:] #Store ccdnum as "c42" -> 42
+                            res['x_exp'][0, i_band, i_cutout]  = o[i_band][i_cutout].meta['orig_col']
+                            res['y_exp'][0, i_band, i_cutout]  = o[i_band][i_cutout].meta['orig_row']                  
+                
+                elif tmp is None:
+                    res = tmp
+                               
             except GMixRangeError as e:
                 logger.debug(" metacal error: %s", str(e))
                 res = None
 
             if res is not None:
-                if mcal_config['custom']['goodfrac']:
-                    res['good_frac'] = np.average(good_frac, weights = weight) #store mean good_fraction per object
-                else:
-                    res['good_frac'] = 1 #Else, we're assuming image is "perfect" (completely unmasked) == 1
                 data.append(res)
 
         if len(data) > 0:
